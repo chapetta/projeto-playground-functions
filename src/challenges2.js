afea30f8c38305e6a5e1d09172aa41aa965a22cd
chapetta <cha.petta@hotmail.com>
2023-05-13 18:22:43 -0300

    Done: requisito 11 - Crie uma função de Número de Telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,24 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// eslint-disable-next-line complexity, sonarjs/cognitive-complexity
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    let counter = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[index] === array[i]) {
+        counter += 1;
+      }
+    }
+    if (array[index] > 9 || array[index] < 0 || counter >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  const areaCode = array.splice(0, 2).join('');
+  const prefix = array.splice(0, 5).join('');
+  const sufix = array.splice(0, 4).join('');
+  return `(${areaCode}) ${prefix}-${sufix}`;
 }
 
 // Desafio 12
